ae26e406b45f1b0d4d12f75b1cd18432c126f2bd
a-poor <a-poor@users.noreply.github.com>
2021-10-15 20:38:06 -0700

    Initial commit

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+export declare function hello(world?: string): string;
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+var WORLD = 'world';
+function hello(world) {
+    if (world === void 0) { world = WORLD; }
+    return "Hello " + world + "! ";
+}
+exports.hello = hello;
+//# sourceMappingURL=index.js.map
